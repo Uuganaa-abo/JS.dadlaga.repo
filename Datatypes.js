983d5a6eb005feb838988396b606c1b98013e84e
Uuganaa-abo <24LP4508@Users-iMac-2.local>
2024-06-06 17:54:03 +0800

    first

---
+++ Datatypes.js
@@ -0,0 +1,15 @@
+let counter = "hello mongolia"
+console. log(typeof(counter)); "string"
+let number = 21
+console. log(typeof(number))
+let num = null
+console.log(typeof(num))
+let tv = false
+console. log(typeof(tv))
+let mongolia
+const a
+let x=11
+console.log(x)
+let array = { 'car', 'pen', 'bowl', 'apple'}
+
+
---
+++ variable.js
@@ -0,0 +1,6 @@
+let num = "name"
+let nam = 11
+let nom = true
+let nem = null 
+let boom
+
